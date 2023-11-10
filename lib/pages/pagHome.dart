b792403aa482cd4430223a1d64c9cde7c11df1d1
EbenCT <cayoeben64@gmail.com>
2023-11-10 00:08:04 -0400

    ProductosAPI

--- lib/pages/pagHome.dart
+++ lib/pages/pagHome.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class PaginaHome extends StatelessWidget {
-  const PaginaHome({super.key});
+class PaginaHome extends StatefulWidget {
+  const PaginaHome({Key? key}) : super(key: key);
 
+  @override
+  _PaginaHomeState createState() => _PaginaHomeState();
+}
+
+class _PaginaHomeState extends State<PaginaHome> {
+  List<Producto> productos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarProductos();
+  }
+
+  Future<void> _cargarProductos() async {
+    final response = await http.get(Uri.parse('http://18.216.45.210/api/productos'));
+
+    if (response.statusCode == 200) {
+      // Si la solicitud fue exitosa, parsea el JSON
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        productos = data.map((item) => Producto.fromJson(item)).toList();
+      });
+    } else {
+      // Si la solicitud no fue exitosa, muestra un mensaje de error
+      throw Exception('Error al cargar productos');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
@@ -13,68 +44,94 @@ class PaginaHome extends StatelessWidget {
           ),
           Container(
             height: 400,
-            child: ListView(
-              children: [
-                Container(
-                  child: Image.network(
-                      "https://hvhindustrial.com/images/frontend_images/blogs/1592499808Electric-Motor.jpg"),
-                ),
-                Column(
-                  children: [
-                    Text("MOTOR ELECTRICO"),
-                    TextButton(onPressed: () {}, child: Text("Comprar"))
-                  ],
-                ),
-                //2
-                Container(
-                  child: Image.network(
-                      "https://cdn-images.motor.es/image/m/720w.webp/fotos-diccionario/2019/11/bujia-tipos-averias-mantenimiento_1573747483.jpg"),
-                ),
-                Column(
-                  children: [
-                    Text("Bujia"),
-                    TextButton(onPressed: () {}, child: Text("Comprar"))
-                  ],
-                ),
-                //3
-                Container(
-                  child: Image.network(
-                      "https://stpimagecdn.imgix.net/wp-content/uploads/sites/3/product-images/17078B_01.png?auto=format%2Ccompress&fit=crop&w=600&max-h=600"),
-                ),
-                Column(
-                  children: [
-                    Text("Liquido para dirección hidraulica"),
-                    TextButton(onPressed: () {}, child: Text("Comprar"))
-                  ],
-                )
-              ],
+            child: ListView.builder(
+              itemCount: productos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(productos[index].nombre),
+                  subtitle: Text('\$${productos[index].precioVenta.toString()}'),
+                  onTap: () {
+                    _mostrarDetalles(productos[index]);
+                  },
+                );
+              },
             ),
           ),
         ],
       ),
     );
   }
-}
 
-Widget _swiper() {
-  return Container(
-    width: double.infinity,
-    height: 300,
-    child: Swiper(
-      viewportFraction: 0.8,
-      scale: 0.9,
-      itemBuilder: (BuildContext context, int index) {
-        return Image.network(
-          imagenes[index],
-          fit: BoxFit.fill,
+  Widget _swiper() {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.9,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            imagenes[index],
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: imagenes.length,
+        pagination: SwiperPagination(),
+        control: SwiperControl(),
+      ),
+    );
+  }
+
+  void _mostrarDetalles(Producto producto) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(producto.nombre),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Descripción: ${producto.descripcion}'),
+              Text('Precio: \$${producto.precioVenta.toString()}'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cerrar'),
+            ),
+          ],
         );
       },
-      itemCount: imagenes.length,
-      pagination: SwiperPagination(),
-      control: SwiperControl(),
-    ),
+    );
+  }
+}
+
+class Producto {
+  final int id;
+  final String nombre;
+  final String descripcion;
+  final double precioVenta;
+
+  Producto({
+    required this.id,
+    required this.nombre,
+    required this.descripcion,
+    required this.precioVenta,
+  });
+
+  factory Producto.fromJson(Map<String, dynamic> json) {
+  return Producto(
+    id: json['id'],
+    nombre: json['nombre'],
+    descripcion: json['descripcion'],
+    precioVenta: double.parse(json['precio_venta'].toString()),
   );
 }
+}
+
 
 List<String> imagenes = [
   "https://www.agsa.com/cdn/shop/products/GATO2TN.jpg?v=1646768569",
